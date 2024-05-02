1618f8f0555a637c4fbebde51a1061dd2c7649c8
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-05-02 20:29:29 +0600

    none

--- controllers/Desktop-View-Controllers/Desktop-question-Controller.js
+++ controllers/Desktop-View-Controllers/Desktop-question-Controller.js
@@ -1,15 +1,15 @@
 const dataBase = require('../../models/DB');
 const path = require('path');
 
-exports.userQuestion = (request, response)=>{
-    if(request.session.isAdminLoggedIn){
-        
+exports.userQuestion = (request, response) => {
+    if (request.session.isAdminLoggedIn) {
+
         // /Desktop/ram-:ramBrand
         let sql = '';
         const date1 = request.query.from;
         const date2 = request.query.to;
 
-        if(date1 == 0){
+        if (date1 == 0) {
             sql = `
             select 
             users.first_name as f,
@@ -24,35 +24,40 @@ exports.userQuestion = (request, response)=>{
             on user_questions.user_id = users.user_id
             where user_questions.answers is not null;
             `;
-        }else{
+        } else {
             sql = `
-            select 
-            users.first_name as f,
-            users.last_name as l,
-            users.email as email,
-            user_questions.product_category as category,
-            user_questions.product_id as pId,
-            user_questions.user_question as question,
-            user_questions.answers as answer
+            SELECT 
+    users.first_name AS f,
+    users.last_name AS l,
+    users.email AS email,
+    user_questions.product_category AS category,
+    user_questions.product_id AS pId,
+    user_questions.user_question AS question,
+    user_questions.answers AS answer
+    FROM 
+    users 
+    JOIN 
+    user_questions ON user_questions.user_id = users.user_id
+    WHERE 
+    user_questions.answers IS NOT NULL 
+    AND user_questions.answers != '' 
+    AND user_questions.question_date BETWEEN '${date1}' AND '${date2}';
 
-            from users join user_questions 
-            on user_questions.user_id = users.user_id
-            where user_questions.question_date between '${date1}' and '${date2} and user_questions.answers is not null ';
             `
         }
 
-        dataBase.query(sql, (error, dataList)=>{
-            if(error){
+        dataBase.query(sql, (error, dataList) => {
+            if (error) {
                 response.status(500).send('error on data fetching')
-            }else{
-                console.log(date1 + " "+ date2);
+            } else {
+                console.log(date1 + " " + date2);
                 response.render(path.join(__dirname, "/../../public/Admin-users-questions.ejs"), { dataList, date1, date2 })
             }
         });
 
 
 
-    }else{
+    } else {
         response.redirect('/');
     }
 
