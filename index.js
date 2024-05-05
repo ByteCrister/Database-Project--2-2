c9e764886c479779e55ea67ff95d3019f475cb1f
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-05-05 18:57:33 +0600

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || process.env.PORT2;
+const PORT = process.env.PORT1 || process.env.PORT2;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on - http://localhost:${PORT}`);
