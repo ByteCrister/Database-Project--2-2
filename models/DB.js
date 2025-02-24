0a7d3c20c492060e2afd22fcd3aa7e65a1c955c3
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-24 13:11:59 +0600

    vercel change

--- models/DB.js
+++ models/DB.js
@@ -6,7 +6,18 @@ const dataBase = mysql.createConnection({
     user: process.env.DB_user,
     password: process.env.DB_password,
     database: process.env.DB,
-    port: process.env.PORT
+    port: process.env.PORT,
+    connectTimeout: 10000, // 10 seconds
+    acquireTimeout: 10000, // 10 seconds
+    timeout: 60000
 });
+
+dataBase.connect((err) => {
+    if (err) {
+      console.error("Database connection failed:", err);
+      return;
+    }
+    console.log("Connected to MySQL Database!");
+  });
 
 module.exports =  dataBase ;
