be6360293f220b1b2c64e4ef0b545dfa9e35267c
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-02-24 12:53:05 +0600

    vercel change

--- app.js
+++ app.js
@@ -81,6 +81,8 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 // app.get('/', (req, res) => {
